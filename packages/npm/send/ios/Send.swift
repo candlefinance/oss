dbebd71f0b7292e215f7f22f9c9d0eb6594677d4
Liam Butler-Lawrence <liamdunn@icloud.com>
2024-05-03 18:28:06 -0600

    fix(ios): handle unwrapped optionals

--- packages/npm/send/ios/Send.swift
+++ packages/npm/send/ios/Send.swift
@@ -4,25 +4,37 @@ private let responseError = "Your request received a response, but it couldn't b
 private let unknownError = "Something went wrong. Please file an issue on GitHub or try again."
 
 private func bodyIsUTF8(contentTypeHeader: String?, utf8ContentTypes: [String]) -> Bool {
-    guard let contentType = contentTypeHeader?.split(separator: ";")[0] else {
+    guard let contentType = contentTypeHeader?.split(separator: ";").first else {
         return false
     }
     return utf8ContentTypes.contains(String(contentType))
 }
 
+enum URLConstructionError: Error {
+    case invalidBaseURL
+    case invalidPathOrQueryParameters
+}
+
+enum URLRequestConstructionError: Error {
+    case invalidBaseURL
+    case invalidPathOrQueryParameters
+    case invalidUTF8Body
+    case invalidBase64Body
+}
+
 @propertyWrapper
-struct MustBePresent<Value> {
+struct RequiredKey<Value> {
     var wrappedValue: Value?
 }
 
-extension MustBePresent: Decodable where Value: Decodable {
+extension RequiredKey: Decodable where Value: Decodable {
     init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         wrappedValue = container.decodeNil() ? nil : try container.decode(Value.self)
     }
 }
 
-extension MustBePresent: Encodable where Value: Encodable {
+extension RequiredKey: Encodable where Value: Encodable {
     func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         try container.encode(wrappedValue)
@@ -35,11 +47,13 @@ struct Request: Decodable {
     let queryParameters: [String: String]
     let headerParameters: [String: String]
     let method: String
-    @MustBePresent var body: String?
+    @RequiredKey var body: String?
     let utf8ContentTypes: [String]
     
-    var url: URL {
-        var urlComponents = URLComponents(string: baseURL)!
+    var url: Result<URL, URLRequestConstructionError> {
+        guard var urlComponents = URLComponents(string: baseURL) else {
+            return .failure(.invalidBaseURL)
+        }
         urlComponents.path = path
         
         if !queryParameters.isEmpty {
@@ -48,45 +62,80 @@ struct Request: Decodable {
             }
         }
         
-        return urlComponents.url!
+        guard let url = urlComponents.url else {
+            return .failure(.invalidPathOrQueryParameters)
+        }
+        return .success(url)
     }
     
-    var urlRequest: URLRequest {
-        var request = URLRequest(url: url)
-        request.httpMethod = method
-        
-        for (key, value) in headerParameters {
-            request.setValue(value, forHTTPHeaderField: key)
-        }
-        
-        if let body {
-            let contentTypeHeader = headerParameters.first(where: { $0.key.caseInsensitiveCompare("Content-Type") == .orderedSame })?.value
-            if bodyIsUTF8(contentTypeHeader: contentTypeHeader, utf8ContentTypes: utf8ContentTypes) {
-                request.httpBody = body.data(using: .utf8)!
-            } else {
-                request.httpBody = Data(base64Encoded: body)!
+    var urlRequest: Result<URLRequest, URLRequestConstructionError> {
+        return url.flatMap { url in
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = method
+            
+            for (key, value) in headerParameters {
+                urlRequest.setValue(value, forHTTPHeaderField: key)
             }
+            
+            if let body {
+                let contentTypeHeader = headerParameters.first(where: { $0.key.caseInsensitiveCompare("Content-Type") == .orderedSame })?.value
+                if bodyIsUTF8(contentTypeHeader: contentTypeHeader, utf8ContentTypes: utf8ContentTypes) {
+                    guard let utf8Body = body.data(using: .utf8) else {
+                        return .failure(.invalidUTF8Body)
+                    }
+                    urlRequest.httpBody = utf8Body
+                } else {
+                    guard let base64Body = Data(base64Encoded: body) else {
+                        return .failure(.invalidBase64Body)
+                    }
+                    urlRequest.httpBody = base64Body
+                }
+            }
+            return .success(urlRequest)
         }
-        return request
     }
+}
+
+enum ResponseConstructionError: Error {
+    case invalidHeaderParameters
+    case invalidUTF8Body
 }
 
 struct Response: Encodable {
     let statusCode: Int
     let headerParameters: [String: String]
-    @MustBePresent var body: String?
+    @RequiredKey var body: String?
     
-    init(request: Request, data: Data, httpURLResponse: HTTPURLResponse) {
-        statusCode = httpURLResponse.statusCode
-        headerParameters = httpURLResponse.allHeaderFields as! [String: String]
+    init(statusCode: Int, headerParameters: [String: String], body: String?) {
+        self.statusCode = statusCode
+        self.headerParameters = headerParameters
+        self.body = body
+    }
+    
+    static func make(request: Request, data: Data, httpURLResponse: HTTPURLResponse) -> Result<Response, ResponseConstructionError> {
+        guard let headerParameters = httpURLResponse.allHeaderFields as? [String: String] else {
+            return .failure(.invalidHeaderParameters)
+        }
         
         let bodyIsUTF8 = bodyIsUTF8(
             contentTypeHeader: httpURLResponse.value(forHTTPHeaderField: "Content-Type"),
             utf8ContentTypes: request.utf8ContentTypes
         )
-        body = bodyIsUTF8
-        ? String(data: data, encoding: .utf8)!
-        : data.base64EncodedString()
+        let body: String?
+        if (bodyIsUTF8) {
+            guard let utf8Body = String(data: data, encoding: .utf8) else {
+                return .failure(.invalidUTF8Body)
+            }
+            body = utf8Body
+        } else {
+            body = data.base64EncodedString()
+        }
+        
+        return .success(Response(
+            statusCode: httpURLResponse.statusCode,
+            headerParameters: headerParameters,
+            body: body
+        ))
     }
 }
 
@@ -114,21 +163,31 @@ class Send: NSObject {
                 }
                 let request = try JSONDecoder().decode(Request.self, from: requestData)
                 
-                let (data, urlResponse) = try await session.data(for: request.urlRequest)
-                guard let httpURLResponse = urlResponse as? HTTPURLResponse else {
-                    reject("@candlefinance.send.response_not_http_url_response", responseError, nil)
-                    return
+                switch request.urlRequest {
+                case .failure(let urlRequestConstructionError):
+                    return reject("@candlefinance.send.request.\(urlRequestConstructionError)", requestError, nil)
+                    
+                case .success(let urlRequest):
+                    let (data, urlResponse) = try await session.data(for: urlRequest)
+                    guard let httpURLResponse = urlResponse as? HTTPURLResponse else {
+                        reject("@candlefinance.send.response.not_http_url_response", responseError, nil)
+                        return
+                    }
+                    switch Response.make(
+                        request: request,
+                        data: data,
+                        httpURLResponse: httpURLResponse
+                    ) {
+                    case .failure(let responseConstructionError): 
+                        return reject("@candlefinance.send.\(responseConstructionError)", requestError, nil)
+                    case .success(let response):
+                        let responseData = try JSONEncoder().encode(Result<Response, Error>.success(response))
+                        guard let stringifiedResponse = String(data: responseData, encoding: .utf8) else {
+                            return reject("@candlefinance.send.response_data_not_utf8", responseError, nil)
+                        }
+                        resolve(stringifiedResponse)
+                    }
                 }
-                let response = Response(
-                    request: request,
-                    data: data,
-                    httpURLResponse: httpURLResponse
-                )
-                let responseData = try JSONEncoder().encode(response)
-                guard let stringifiedResponse = String(data: responseData, encoding: .utf8) else {
-                    return reject("@candlefinance.send.response_data_not_utf8", responseError, nil)
-                }
-                resolve(stringifiedResponse)
                 
             } catch let decodingError as DecodingError {
                 reject("@candlefinance.send.stringified_request_invalid", requestError, decodingError)
