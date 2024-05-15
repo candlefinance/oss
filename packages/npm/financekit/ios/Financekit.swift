7abf15194d8d6b9b92d20025bbe62b98adddc5ad
Gary Tokman <hello@garytokman.me>
2024-05-15 11:36:25 -0400

    feat(ios): initial commit

---
+++ packages/npm/financekit/ios/Financekit.swift
@@ -0,0 +1,8 @@
+@objc(Financekit)
+class Financekit: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
