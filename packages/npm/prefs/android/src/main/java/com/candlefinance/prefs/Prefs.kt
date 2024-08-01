dbc332223d2253260bf9f858fbec764fe0bdfcaa
Liam Butler-Lawrence <liamdunn@icloud.com>
2024-07-31 20:36:33 -0600

    fix(prefs): add ios impl

--- packages/npm/prefs/android/src/main/java/com/candlefinance/prefs/Prefs.kt
+++ packages/npm/prefs/android/src/main/java/com/candlefinance/prefs/Prefs.kt
@@ -18,6 +18,10 @@ class PrefsModule(reactContext: ReactApplicationContext) :
   fun getPref(key: String, promise: Promise) {
     try {
       promise.resolve(sharedPreferences.getString(key, null))
+    } catch (e: ClassCastException) {
+      promise.reject(
+        CODE_NON_STRING_VALUE, MESSAGE_NON_STRING_VALUE, e
+      )
     } catch (e: Exception) {
       promise.reject(
         CODE_UNEXPECTED, MESSAGE_UNEXPECTED, e
@@ -72,10 +76,13 @@ class PrefsModule(reactContext: ReactApplicationContext) :
     const val NAME = "Prefs"
 
     const val CODE_EDIT_COMMIT_FAILED = "@candlefinance.prefs.edit_commit_failed"
+    const val CODE_NON_STRING_VALUE = "@candlefinance.prefs.non_string_value"
     const val CODE_UNEXPECTED = "@candlefinance.prefs.unexpected"
 
     const val MESSAGE_EDIT_COMMIT_FAILED =
       "SharedPreferences edit could not be committed. Please file an issue on GitHub or try again."
+    const val MESSAGE_NON_STRING_VALUE =
+      "SharedPreferences value was found, but is not a string. You can use deletePref() to remove the existing value and try again. If you're setting this value from native Android code, make sure to use putString()."
     const val MESSAGE_UNEXPECTED =
       "Something went wrong. Please file an issue on GitHub or try again."
   }
--- packages/npm/prefs/ios/Prefs.mm
+++ packages/npm/prefs/ios/Prefs.mm
@@ -2,17 +2,9 @@
 
 @interface RCT_EXTERN_MODULE(Prefs, NSObject)
 
-RCT_EXTERN_METHOD(requestAuthorization:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(authorizationStatus:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(transactions:(NSString *)stringifiedQuery withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(transactionHistory:(NSString *)stringifiedParams withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(accounts:(NSString *)stringifiedQuery withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(accountHistory:(NSString *)stringifiedParams withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(accountBalances:(NSString *)stringifiedQuery withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(accountBalanceHistory:(NSString *)stringifiedParams withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(getPref:(NSString *)key withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(setPref:(NSString *)key toValue:(NSString *)value withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(deletePref:(NSString *)key withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
 
 + (BOOL)requiresMainQueueSetup
 {
