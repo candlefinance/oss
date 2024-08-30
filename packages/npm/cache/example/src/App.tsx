35f7dbf129feff9824243472a3bea922710f6a2c
Gary Tokman <yo@6ary.com>
2024-08-30 13:02:07 -0400

    feat: add benchmark against mmkv

--- packages/npm/cache/example/src/App.tsx
+++ packages/npm/cache/example/src/App.tsx
@@ -10,6 +10,9 @@ import {
   writeAsync,
 } from '@candlefinance/cache';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { MMKV } from 'react-native-mmkv';
+
+export const storage = new MMKV();
 
 const benchmark = () => {
   const start = Date.now();
@@ -52,6 +55,29 @@ const benchmarkAsync = async () => {
   const start3 = Date.now();
   for (let i = 0; i < 10000; i++) {
     await removeAsync(`key${i}`);
+  }
+  const end3 = Date.now();
+  console.log('remove', end3 - start3);
+};
+
+const benchmarkMMKV = () => {
+  const start = Date.now();
+  for (let i = 0; i < 10000; i++) {
+    storage.set(`key${i}`, `value${i}`);
+  }
+  const end = Date.now();
+  console.log('write', end - start);
+
+  const start2 = Date.now();
+  for (let i = 0; i < 10000; i++) {
+    storage.getString(`key${i}`);
+  }
+  const end2 = Date.now();
+  console.log('read', end2 - start2);
+
+  const start3 = Date.now();
+  for (let i = 0; i < 10000; i++) {
+    storage.delete(`key${i}`);
   }
   const end3 = Date.now();
   console.log('remove', end3 - start3);
@@ -112,6 +138,13 @@ export default function App() {
         onPress={() => {
           console.log('benchmark async read/write/delete 10000 times');
           benchmarkAsync();
+        }}
+      />
+      <Button
+        title="Benchmark MMKV"
+        onPress={() => {
+          console.log('benchmark MMKV read/write/delete 10000 times');
+          benchmarkMMKV();
         }}
       />
     </View>
