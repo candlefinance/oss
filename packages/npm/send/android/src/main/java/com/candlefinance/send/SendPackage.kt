0ed31d2ef4ac6bb2882258aab65a23cc1e30ef3b
Gary Tokman <yo@6ary.com>
2024-09-02 16:46:33 -0400

    fix: revert android changes

--- packages/npm/send/android/src/main/java/com/candlefinance/send/SendPackage.kt
+++ packages/npm/send/android/src/main/java/com/candlefinance/send/SendPackage.kt
@@ -1,28 +1,14 @@
 package com.candlefinance.send
 
-import android.content.Context
-import android.util.Log
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.model.ReactModuleInfoProvider
-import com.margelo.nitro.core.HybridObjectRegistry
+import com.facebook.react.uimanager.ViewManager
 
-class SendPackage() : TurboReactPackage() {
-  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
-    return null
-  }
+class SendPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
+    listOf(SendModule(reactContext))
 
-  init {
-//    HybridObjectRegistry.registerHybridObjectConstructor("Send") {
-//      Log.i("YEET", "initializing Send...")
-//      val f: SendModule = SendModule(Context())
-//      Log.i("YEET", "done Send!")
-//      f
-//    }
-  }
-
-  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
-    return ReactModuleInfoProvider { HashMap() }
-  }
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> =
+    emptyList()
 }
